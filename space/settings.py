73e888f9d02b691a1c9752151c0c7153aff2aba1
mohammeddanishrabbani <mohammeddanishrabbani@gmail.com>
2021-12-01 21:47:04 +0530

    Automatic configuration with django-heroku

--- space/settings.py
+++ space/settings.py
@@ -134,3 +134,5 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
