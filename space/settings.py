f90ba02cfafe627d07837791c3c7e5c13b0b4d44
mohammeddanishrabbani <mohammeddanishrabbani@gmail.com>
2021-12-01 23:19:22 +0530

    Remove a hardcoded Django secret key

--- space/settings.py
+++ space/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l8ai7v!)jf69!nwf%z7)er6m=)n2p4g9%b2e%_s*3@obdgv@6x'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
