0b3fbdefdcbeb9df673d04cf47a384c6bf55aded
mohammeddanishrabbani <mohammeddanishrabbani@gmail.com>
2021-12-01 23:47:24 +0530

    aa

--- space/settings.py
+++ space/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 from django.db.models import base
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -126,12 +126,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-#STATICFILES_DIRS = [
-#    BASE_DIR / 'static'
-#]
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
